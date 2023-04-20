79ea68e111a2642928a70eb97a9cee2c310e02f4
Sujana <102030769+sujana95-26@users.noreply.github.com>
2023-04-20 16:30:27 +0100

    Update dictionary_exercise.py

--- dictionary_exercise.py
+++ dictionary_exercise.py
@@ -1,5 +1,5 @@
 # make a books dictionary, with 3 authors as keys and multiple books as values. least 2 books for each author
-# usse an input()asking for author name and print as a string the list of books by that
+# use an input()asking for author name and print as a string the list of books by that
 #author. use the .join()method
 
 books_dictionary = {"Stephanie Myers": ["Twilight", "New Moon", "Eclipse"], "Jane Austen": ["Pride and Prejudice", "Sense and sensibilities", "Mansfield Park"], "Stephen King": ["The Shining", "Fairy Tale", "It"]}
