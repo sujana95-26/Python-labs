3ebf24f76bd06f2c414f991e53e46fcb04b1e961
Sujana <102030769+sujana95-26@users.noreply.github.com>
2023-04-20 14:34:28 +0100

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,23 @@
+print ('Hello World!')
+
+username = "Bob"
+age = 32
+print (username, 'is', age, 'years old')
+
+username = "Bob"
+age = "32"
+print(username+"is"+age+"years old")
+
+print(username," is "+age+" years old")
+
+#username=input("Please enter your name")
+#age = input("Please enter your age")
+#print(username, "is",age, "years old")8
+
+length= int(input("length?: "))
+width = int(input("width?: "))
+
+Perimeter = (length*2)+(width*2)
+print ("The perimeter of the rectangle is", Perimeter)
+
+
