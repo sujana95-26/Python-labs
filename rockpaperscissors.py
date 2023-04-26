ea412253509444b74b21a52875f7d437c30e7cc2
Sujana <102030769+sujana95-26@users.noreply.github.com>
2023-04-26 10:58:55 +0100

    Add files via upload

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,4 +1,7 @@
+
 import random
+
+
 
 def play_game():
     print("Play Rock-Paper-Scissors!")
@@ -7,18 +10,35 @@ def play_game():
     computer_choice = random.choice(options)
     print(f"You chose {user_choice}, computer chose {computer_choice}")
     if user_choice == computer_choice:
-        print("Tie!")
+        return "Tie!"
     elif user_choice == "rock" and computer_choice =="scissors":
-        print("User won")
-
+        return "User won"
     elif user_choice == "paper" and computer_choice == "rock":
-        print("User won")
+        return "User won"  
     elif user_choice == "scissors" and computer_choice == "paper":
-        print("User won")
+        return "User won" 
     else:
-        print("Computer won") 
-while True:
-    play_game()
+        return "Computer won"
+
+num_rounds = 0  
+user_wins = 0
+computer_wins = 0
+play_again = "y"    
+
+while play_again == "y":
+    num_rounds += 1
+    result = play_game()
+    if result == "user won":
+        user_wins +=1
+        print("user wins")
+    elif result =="computer":
+        computer_wins +=1
+        print("computer wins")
+    else:
+        print("tie game")
+    print("The number of rounds played: ",num_rounds)
+    print("User wins: ", user_wins)
+    print("Computer wins:", computer_wins)
     play_again = input("Do you want to play again? (y/n): ")
-    if play_again.lower() == "y":
-        continue
+    if play_again.lower() != "y":
+        break
